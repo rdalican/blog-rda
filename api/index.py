c22de3829033bef344cd5a4e3830f861d049ff0d
rdalican <roberto.dalicandro@gmail.com>
2025-06-03 12:48:56 +0200

    Aggiunta integrazione con Notion per i post del blog

--- api/index.py
+++ api/index.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, abort
 import os
+from dotenv import load_dotenv
+from notion_client import Client
+from .notion_client import get_blog_posts, get_post_by_slug
+
+load_dotenv()
 
 app = Flask(__name__, 
            template_folder='../templates',  # Point to templates in parent directory
@@ -7,7 +12,20 @@ app = Flask(__name__,
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    posts = get_blog_posts()
+    return render_template('index.html', posts=posts[:3])  # Show latest 3 posts
+
+@app.route('/blog')
+def blog():
+    posts = get_blog_posts()
+    return render_template('blog.html', posts=posts)
+
+@app.route('/blog/<slug>')
+def post(slug):
+    post = get_post_by_slug(slug)
+    if not post:
+        abort(404)
+    return render_template('post.html', post=post)
 
 @app.route('/api/health')
 def health():
